e4cc129dc57f28ee371cbe3bec93a523bd9576db
Martin Albert <Gadriel@gmail.com>
2019-02-27 23:38:24 -0700

    Food bowls set up, only set position once.

--- Assets/Scripts/UpdateTransformFromEntity.cs
+++ Assets/Scripts/UpdateTransformFromEntity.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class UpdateTransformFromEntity : MonoBehaviour
 {
     public Entity Parent;
+    public bool SetOnce = false;
 
     private EntityManager _manager;
 
     private void Start()
     {
         _manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        if (SetOnce)
+        {
+            if (Parent != null)
+            {
+                var pos = _manager.GetComponentData<Position>(Parent);
+                var rot = _manager.GetComponentData<Rotation>(Parent);
+
+                var t = transform;
+                t.SetPositionAndRotation(pos.Value, rot.Value);
+            }
+
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
